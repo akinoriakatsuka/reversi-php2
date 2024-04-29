7f3dbbeec3ef2ae045a45301e474614e5f0923e4
akinoriakatsuka <akinoriakatsuka@gmail.com>
2024-04-29 23:44:30 +0900

    呼び出し元プログラムで初期配置を定義

--- main.php
+++ main.php
@@ -5,15 +5,20 @@ require_once __DIR__ . '/vendor/autoload.php';
 use App\Game\Game;
 use App\Game\Board;
 use App\Game\Color;
+use App\Game\Stone;
 
 $board = new Board();
+$board->setStone(3, 3, new Stone(Color::WHITE));
+$board->setStone(3, 4, new Stone(Color::BLACK));
+$board->setStone(4, 3, new Stone(Color::BLACK));
+$board->setStone(4, 4, new Stone(Color::WHITE));
 $game = new Game($board);
 
 while (true) {
     $turn = $game->getTurn() === Color::BLACK ? '黒' : '白';
     echo $turn . ' の手番です' . PHP_EOL;
     echo $game->currentBoard();
-    // 入力を受け付ける
+
     echo '石を置く場所を入力してください（qで途中終了）: ';
 
     $input = trim(fgets(STDIN));
@@ -36,7 +41,6 @@ while (true) {
 
     $game->play($row, $col);
     echo PHP_EOL;
-    // ゲームの処理
     if ($game->finished()) {
         break;
     }
