3be0aea40c06b544b373a4583a82321b59571339
akinoriakatsuka <akinoriakatsuka@gmail.com>
2024-04-29 23:42:08 +0900

    過剰なアサーションの削除

--- tests/GameTest.php
+++ tests/GameTest.php
@@ -47,25 +47,12 @@ final class GameTest extends TestCase
     {
         $board = new Board(1, 8);
         $game = new Game($board);
+
         $game->play(0, 0);
         $this->assertSame($game->getTurn(), Color::WHITE);
-        $currentBoard = $game->currentBoard();
-        $expected = <<<EOL
-          a b c d e f g h
-        1 o - - - - - - -
-
-        EOL;
-        $this->assertSame($currentBoard, $expected);
 
         $game->play(0, 1);
         $this->assertSame($game->getTurn(), Color::BLACK);
-        $currentBoard = $game->currentBoard();
-        $expected = <<<EOL
-          a b c d e f g h
-        1 o x - - - - - -
-
-        EOL;
-        $this->assertSame($currentBoard, $expected);
 
         $game->play(0, 2);
         $this->assertSame($game->getTurn(), Color::WHITE);
