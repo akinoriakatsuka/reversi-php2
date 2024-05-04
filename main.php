8d8091b66c6d124052d11e94bb2c4575d08c7080
akinoriakatsuka <akinoriakatsuka@gmail.com>
2024-05-04 17:18:15 +0900

    スクリプト実行時に引数を渡すことで、対戦モードを切り替えられるように変更

--- main.php
+++ main.php
@@ -13,6 +13,22 @@ $output = new ConsoleOutput();
 $black = new HumanPlayer($game);
 $white = new CPUPlayer($game);
 
+if (isset($argv[1])) {
+    if ($argv[1] === 'cpu') {
+        $black = new CPUPlayer($game);
+    } elseif($argv[1] === 'human') {
+        $black = new HumanPlayer($game);
+    }
+}
+
+if (isset($argv[2])) {
+    if ($argv[2] === 'cpu') {
+        $white = new CPUPlayer($game);
+    } elseif($argv[2] === 'human') {
+        $white = new HumanPlayer($game);
+    }
+}
+
 $dealer = new GameDealer($game, $output, $black, $white);
 
 $dealer->deal();
