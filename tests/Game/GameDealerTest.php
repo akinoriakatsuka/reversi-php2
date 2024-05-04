ed71e5dfb8c32bbaaf42ed798856122e9dea67cf
akinoriakatsuka <akinoriakatsuka@gmail.com>
2024-05-05 01:12:02 +0900

    GameDealerのテストをモックを使って行うように変更

--- tests/Game/GameDealerTest.php
+++ tests/Game/GameDealerTest.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace Tests\Game;
 
-use App\Player\CPUPlayer;
-use App\Game\GameFactory;
 use App\Game\GameDealer;
+use App\Game\Game;
+use App\Game\Board;
+use App\Game\Stone;
+use App\Game\Color;
 use App\Output\ConsoleOutput;
+use App\Player\PlayerInterface;
 use PHPUnit\Framework\TestCase;
 
 class GameDealerTest extends TestCase
@@ -17,12 +20,17 @@ class GameDealerTest extends TestCase
      */
     public function testDeal(): void
     {
-        $game = GameFactory::create();
+        $board = new Board(1, 3);
+        $board->setStone(1, 0, new Stone(Color::BLACK));
+        $board->setStone(1, 1, new Stone(Color::WHITE));
+        $game = new Game($board);
+
         $output_mock = $this->createMock(ConsoleOutput::class);
         $output_mock->expects($this->any())->method('write')->with($this->isType('string'));
 
-        $black = new CPUPlayer($game);
-        $white = new CPUPlayer($game);
+        $black = $this->createStub(PlayerInterface::class);
+        $black->method('chooseCell')->willReturn([1, 2]);
+        $white = $this->createStub(PlayerInterface::class);
         $dealer = new GameDealer($game, $output_mock, $black, $white);
         $dealer->deal();
         $this->assertTrue($game->finished());
